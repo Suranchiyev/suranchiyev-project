1ca7ec077d74821134f4e39bc43090148e0e9bb3
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2020-08-02 10:27:30 -0400

    10 times hi

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,5 +1,15 @@
 public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
+		System.out.println("Hi!");
 	}
 }
